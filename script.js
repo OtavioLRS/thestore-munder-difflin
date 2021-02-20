df13ad36b9cbed8d015b2449f0c27018da560e85
OtavioLRSo <otavioleitesantos@hotmail.com>
2021-02-20 15:23:07 -0300

    frete aleatorio

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("bye cruel world")
+function generateShipping() {
+  let days = Math.floor((Math.random() * 8) + 3)
+  console.log(days)
+  document.querySelector('#shipping').innerHTML = "Seu pedido chegará em ".concat(days).concat(" dias!")
+}
